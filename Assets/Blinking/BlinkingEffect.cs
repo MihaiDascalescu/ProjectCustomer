caa58c872a915802d65a6ac0aaf1d72543802eab
Patrik <pnyilka@icloud.com>
2019-09-17 14:09:26 +0200

    NewExport

---
+++ Assets/Blinking/BlinkingEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingEffect : MonoBehaviour
+{
+    public string topLidAnimation;
+    public string bottomLidAnimation;
+    public GameObject PostProcessingEffect;
+
+    private Animation anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Blinking();
+        PostProcessingEffect.SetActive(true);
+    }
+
+    void Blinking()
+    {
+        anim.Play(topLidAnimation);
+        anim.Play(bottomLidAnimation);
+        
+        
+    }
+    
+}
