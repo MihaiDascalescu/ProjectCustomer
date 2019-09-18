a0bdfcddc22818174d10b9b8c73af23185a7ab93
Mihai Dascalescu <dascalescumihaipetru@gmail.com>
2019-09-18 13:09:19 +0200

    changes to the endGameManager and PlayerCollisions scripts Patrick use EndGameManager

--- Assets/ExportFolder/Scripts/EndGameManager.cs
+++ Assets/ExportFolder/Scripts/EndGameManager.cs
@@ -7,22 +7,56 @@ public class EndGameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private PlayerCollisions playerCollisions;
+    private AlcoholMeter alcoholMeter;
+    private Objectives objectives;
+
+    public GameObject BuildingIsHitPenalty;
+    public GameObject AccidentWith004Penalty;
+    public GameObject ExtremelyDrunkPenalty;
     //public  GameObject
     void Start()
     {
         playerCollisions = FindObjectOfType<PlayerCollisions>();
+        alcoholMeter = FindObjectOfType<AlcoholMeter>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //For building accident
         if (playerCollisions.buildingIsHit)
         {
 
-        }
-        if (playerCollisions.carIsHit)
+        }    
+        //For car accident with small alcohol levels
+        if (playerCollisions.carIsHit && alcoholMeter.AlcoholLevel > 0 && alcoholMeter.AlcoholLevel < 40) 
         {
-            
+
+        }
+        //For car accident with medium alcohol levels
+        if (playerCollisions.carIsHit && alcoholMeter.AlcoholLevel > 40 && alcoholMeter.AlcoholLevel < 80)
+        {
+
+        }
+        //For car accident with high alcohol levels
+        if (playerCollisions.carIsHit && alcoholMeter.AlcoholLevel > 80)
+        {
+
+        }
+        //For when the time is up
+        if (playerCollisions.timeIsUp)
+        {
+
+        }
+        //For when really finish the game
+        if (objectives.ThirdObjectiveIsEnabled)
+        {
+
+        } 
+        //For when you finish the game but with level of alcohol
+        if(objectives.ThirdObjectiveIsEnabled && alcoholMeter.AlcoholLevel > 0)
+        {
+
         }
     }
   
